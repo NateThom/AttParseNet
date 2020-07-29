4ae0cb495b154f6e66e5422e574b1c565383b0b9
Nathan C Thom <nathanthom@nevada.unr.edu>
2020-07-29 13:43:49 -0700

    Completed comments for attparsenet.py

--- attparsenet.py
+++ attparsenet.py
@@ -3,7 +3,6 @@ import os
 import torch
 import time
 import cv2
-import random
 from tqdm import tqdm
 
 import numpy as np
@@ -14,15 +13,14 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms.functional as TF
 
-from torch.autograd import Variable
 from skimage import transform
 from torch.utils.data import Dataset, DataLoader, Subset
-from torchvision import transforms, utils as torchvision_utils
+from torchvision import transforms
 from PIL import Image
 
 
 class Rescale(object):
-    """Rescale the image in a sample to a given size.
+    """Rescale the image and masks in a sample to a given size.
     Args:
         output_size (tuple or int): Desired output size. If tuple, output is matched to output_size. If int, smaller
         of image edges is matched to output_size keeping aspect ratio the same.
@@ -33,12 +31,13 @@ class Rescale(object):
         self.output_size = output_size
 
     def __call__(self, sample):
-        #Create copies of the variables that will be transformed from the values passed in
+        # Create copies of the variables that will be transformed from the values passed in
         image, masks = sample['image'], sample['masks']
 
+        # Get the height and width of the image passed in
         h, w = image.shape[:2]
 
-	# Veryify that the variables passed in conform to the expected format
+        # Verify that the variables passed in conform to the expected format
         if isinstance(self.output_size, int):
             if h > w:
                 new_h, new_w = self.output_size * h / w, self.output_size
@@ -48,22 +47,24 @@ class Rescale(object):
             new_h, new_w = self.output_size
         new_h, new_w = int(new_h), int(new_w)
 
-	# THIS SEEMS LIKE SOMETHING THAT SHOULD BE ADDRESSED IN THE README
-	#     AND INCLUDED IN THE UTILS.PY FILE
+        # THIS SEEMS LIKE SOMETHING THAT SHOULD BE ADDRESSED IN THE README
+        #     AND INCLUDED IN THE UTILS.PY FILE
         # if images are not resized before input then uncomment the next line
 
         # img = transform.resize(image, (new_h, new_w))
 
         msk = []
-        # Iterate over all 40 segment labels in the masks variable
+        # Iterate over all 40 segment labels in the masks variable, resize them, and append them to the msk list
         for index in range(len(masks)):
             msk.append(transform.resize(masks[index], (new_h, new_w)))
         msk = np.asarray(msk)
 
+        # Return the resized image and masks. Note that attributes was not transformed
         return {'image': image, 'attributes': sample['attributes'], 'masks': msk}
 
-class RandomCrop(object):
-    """Crop randomly the image in a sample.
+
+class AttParseNetRandomCrop(object):
+    """Crop randomly the image and masks in a sample. The masks are then resized to dimensions of 96x76
 
     Args:
         output_size (tuple or int): Desired output size. If int, square crop
@@ -71,6 +72,7 @@ class RandomCrop(object):
     """
 
     def __init__(self, output_size):
+        # Ensure that the arguments passed in are of the expected format
         assert isinstance(output_size, (int, tuple))
         if isinstance(output_size, int):
             self.output_size = (output_size, output_size)
@@ -81,40 +83,52 @@ class RandomCrop(object):
     def __call__(self, sample):
         image, masks = sample['image'], sample['masks']
 
-	#Uncommnet the next three lines for testing: input image before resizing
+        # Uncommnet the next three lines for testing: input image before resizing
         # image = image.numpy()
         # image = image.transpose(1, 2, 0)
         # cv2.imwrite("image_test.png", image)
 
+        # Get the height and width of the image in sample
         image_h, image_w = image.shape[1:3]
 
+        # Copy the height and width from output size
         new_image_h, new_image_w = self.output_size
 
+        # Randomly select a point to crop the top and left edge of the image to
         top = np.random.randint(0, image_h - new_image_h)
         left = np.random.randint(0, image_w - new_image_w)
 
+        # Narrow (crop) the image
         image = image.narrow(1, top, new_image_h)
         image = image.narrow(2, left, new_image_w)
 
-        mask_h, mask_w = masks.shape[1:3]
-
+        # Copy the height and width from output size
         new_mask_h, new_mask_w = self.output_size
 
+        # Narrow (crop) the masks to the same randomly selected parameters as image
         masks = masks.narrow(1, top, new_mask_h)
         masks = masks.narrow(2, left, new_mask_w)
 
+        # Resize each of the images to dimensions of 96x76 and ensure that all pixel values are either 0 or 255
         output_masks = None
         for index, mask in enumerate(masks):
+            # Convert to numpy and resize
             mask_np = mask.numpy()
             mask_np = cv2.resize(mask_np, (76, 96))
+
+            # Ensure that all pixel values are either 0 or 255
             mask_np = (mask_np > 0).astype(np.uint8) * 255
+
+            # Convert back to tensor
             mask_np = TF.to_tensor(mask_np)
 
+            # Reconstruct the tensors so that they have a dimension of 40x96x76
             if output_masks is None:
                 output_masks = mask_np
             else:
                 output_masks = torch.cat((mask_np, output_masks))
 
+        # Return the randomly cropped image and masks, note that attributes were not transformed
         return {'image': image, 'attributes': sample['attributes'], 'masks': output_masks}
 
 
@@ -138,10 +152,19 @@ class AttParseNetDataset(Dataset):
     """AttParseNet dataset."""
 
     def __init__(self, args, transform=None):
+        # Save the custom arguments, which are retrieved from the utils.py file
         self.args = args
+
+        # Read the binary attribute labels from the specified file
         self.attr_labels = pd.read_csv(args.attr_label_path, sep=',', skiprows=0, usecols=[n for n in range(1, 41)])
+
+        # Get the paths to each of the input images
         self.input_filenames = pd.read_csv(args.attr_label_path, sep=',', skiprows=0, usecols=[0])
+
+        # Get the paths to each of the segment label images (masks)
         self.mask_label_filenames = pd.read_csv(args.mask_label_path, sep=',', usecols=[n for n in range(2, 42)])
+
+        # If there are any transform functions to be called, store them
         self.transform = transform
 
     def __len__(self):
@@ -151,19 +174,26 @@ class AttParseNetDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
+        # Get full path to the current input image
         img_name = os.path.join(self.args.image_path, self.input_filenames.iloc[idx, 0])
+
+        # Read the image into memory, convert it to numpy, create a copy of it, and finally make the image a tensor
         read_image = Image.open(img_name)
         np_image = np.asarray(read_image)
         image = np.copy(np_image)
         image = torch.from_numpy(image)
 
+        # Read in the attribute labels for the current input image
         attributes = self.attr_labels.iloc[idx,]
         attributes = np.asarray(attributes)
+        # Iterate over each of the input images and change and "-1" labels to "0"
         for index in range(len(attributes)):
             if attributes[index] == -1:
                 attributes[index] = 0
+        # Convert the labels to floats, I think that this was necessary for training
         attributes = torch.from_numpy(attributes).float()
 
+        # Read all of the segment label images for the current input image
         masks = None
         for filename in self.mask_label_filenames.iloc[idx,]:
             mask = cv2.imread(os.path.join(self.args.mask_image_path, filename), 0)
@@ -181,7 +211,7 @@ class AttParseNetDataset(Dataset):
 
         return sample
 
-
+# Define the CNN architecture
 class AttParseNet(nn.Module):
     def __init__(self):
         super(AttParseNet, self).__init__()
@@ -224,7 +254,7 @@ def show_batch(batch, show_input=True, show_masks=True):
         for index, image in enumerate(images_batch):
             image = image.numpy()
             image = image.transpose(1, 2, 0)
-            # Note that there are two ways to view an image. Save the image and open it, or 
+            # Note that there are two ways to view an image. Save the image and open it, or
             #      show the image while the program is running. Either uncomment imshow and waitKey
             #      or imwrite
             # cv2.imshow("Input Image", image)
@@ -258,7 +288,7 @@ def plot_loss(args, loss_per_epoch, epochs):
     plt.plot(epochs, loss_per_epoch)
     plt.savefig(args.train_plot_path + f'Pretrain_Loss_Curve_Epoch_{epochs[-1] + 1}')
 
-
+# Call prior to training or testing to see batch load times
 def test_batch_load_time(args, data_loader):
     start_time = time.time()
     for progress_counter, batch in enumerate(data_loader):
@@ -268,11 +298,16 @@ def test_batch_load_time(args, data_loader):
         print(f"Total time: {time.time() - start_time}")
     print("End of test")
 
+# Computes total accuracy, accuracy of positive samples, accuracy of negative samples, precision, and recall of the
+# current batch
+def compute_metrics(attribute_preds, attribute_labels, true_pos_count, true_neg_count, false_pos_count,
+                    false_neg_count):
 
-def compute_metrics(attribute_preds, attribute_labels, true_pos_count, true_neg_count, false_pos_count, false_neg_count):
+    # We use the BCEWithLogits loss function, so the sigmoid needs to be applied before computing our metrics
     attribute_preds = torch.sigmoid(attribute_preds)
     attribute_preds = torch.round(attribute_preds)
 
+    # Remove the predictions from GPU and move to CPU
     attribute_preds_cpu = attribute_preds.detach().to(torch.device("cpu"))
     attribute_labels_cpu = attribute_labels.detach().to(torch.device("cpu"))
 
@@ -306,7 +341,7 @@ def compute_metrics(attribute_preds, attribute_labels, true_pos_count, true_neg_
 
     return precision, recall, accuracy, accuracy_pos, accuracy_neg
 
-
+# Function for outputting metrics. Can output to console, csv, or txt (note comma separated) file
 def output(args, output_preds, iteration_time, start_time, accuracy, accuracy_pos, accuracy_neg, precision, recall,
            console=True, file=False, csv=False):
     ### TO FILE ###
@@ -365,33 +400,40 @@ def output(args, output_preds, iteration_time, start_time, accuracy, accuracy_po
         output_preds_df.to_csv(args.metrics_csv_output_path, sep=',')
     ##############
 
-
+# Trains the model for one epoch
 def train(net, optimizer, criterion1, criterion2, data_loader, start_time):
     running_total_loss = 0.0
     running_bce_loss = 0.0
     running_l2_loss = 0.0
 
+    # Iterate over all batches in an epoch
     for iteration_index, sample_batched in enumerate(data_loader):
         iteration_time = time.time()
 
+        # Get the input images, attribute labels, and masks for each sample in the batch
         inputs, attribute_labels, mask_labels = sample_batched['image'], sample_batched['attributes'], \
                                                 sample_batched['masks']
 
-        show_batch(sample_batched, show_input=True, show_masks=True)
+        #show_batch(sample_batched, show_input=True, show_masks=True)
 
+        # Place the model and batch of data onto the GPU
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         net.to(device)
         inputs, attribute_labels, mask_labels = inputs.to(device), attribute_labels.to(device), mask_labels.to(device)
 
+        # Zero the gradients
         optimizer.zero_grad()
         net.zero_grad()
 
+        # Get a attribute and mask predictions for each sample in the batch
         attribute_preds, mask_preds = net(inputs)
 
+        # Compute loss for attribute prediction and segmentation separately, then add them together
         loss1 = criterion1(attribute_preds, attribute_labels)
         loss2 = 2 * torch.sqrt(criterion2(mask_preds, mask_labels))
         loss = loss1 + loss2
 
+        # Calculate backprop and step
         loss.backward()
         optimizer.step()
 
@@ -440,7 +482,7 @@ def test(net, optimizer, criterion1, criterion2, data_loader):
         loss = loss1 + loss2
         running_total_loss += loss
 
-        #Uncomment the next 5 lines to view the metrics for each batch
+        # Uncomment the next 5 lines to view the metrics for each batch
         # precision, recall, accuracy, accuracy_pos, accuracy_neg = compute_metrics(attribute_preds, attribute_labels,
         #                                                                           true_pos_count, true_neg_count,
         #                                                                           false_pos_count, false_neg_count)
@@ -451,9 +493,13 @@ def test(net, optimizer, criterion1, criterion2, data_loader):
 
 
 def main():
+    # Get args from utils.py
     args = utils.get_args()
 
-    dataset = AttParseNetDataset(args, transform=transforms.Compose([RandomCrop((218,178))]))
+    # Collect dataset and apply transformations
+    dataset = AttParseNetDataset(args, transform=transforms.Compose([AttParseNetRandomCrop((218, 178))]))
+
+    # Establish train, validate, and test splits
     train_indices, val_indices, test_indices = list(range(args.train_size)), \
                                                list(range(args.train_size, args.train_size + args.val_size)), \
                                                list(range(args.train_size + args.val_size, args.all_size))
@@ -471,9 +517,14 @@ def main():
     val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, num_workers=1)
     test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=1)
 
+    # Initialize the model
     net = AttParseNet()
+
+    # Load a model from disk
     if args.load is True:
         net.load_state_dict(torch.load(args.load_path), strict=False)
+
+    # Compute number of parameters in the network
     pytorch_total_params = sum(p.numel() for p in net.parameters())
 
     ########## Parallelization ##########
@@ -490,21 +541,22 @@ def main():
         for i in range(len(params)):
             print(params[i].size())
 
+    # Initialize loss functions and optimizer
     criterion1 = nn.BCEWithLogitsLoss()
     criterion2 = nn.MSELoss()
     optimizer = optim.Adam(net.parameters(), lr=args.lr)
 
-########## TRAIN BY NUM EPOCH ##########
+    ########## TRAIN BY NUM EPOCH ##########
     if args.train_by_num_epoch:
         start_time = time.time()
         min_loss = np.inf
 
         loss_per_epoch = []
 
+        # Train for the number of epochs denoted in utils.py
         for epoch in tqdm(range(args.train_epochs)):
-
+            # Store the total loss over the training epoch
             epoch_loss = train(net, optimizer, criterion1, criterion2, train_loader, start_time)
-
             print(f"Epoch {epoch} Loss: {epoch_loss}")
 
             if args.plot_loss:
@@ -518,9 +570,9 @@ def main():
             epochs = [i for i in range(epoch + 1)]
             plot_loss(args, loss_per_epoch, epochs)
         print("Finished Training!")
-##########
+    ##########
 
-########## TRAIN BY COMP W/ VALIDATION ##########
+    ########## TRAIN BY COMP W/ VALIDATION ##########
     if args.train_by_comparison_with_validation:
 
         start_time = time.time()
@@ -532,6 +584,7 @@ def main():
         training_loss = np.inf
         validation_loss = np.inf
 
+        # Continue training until the training loss is no longer greater than the validation loss
         while training_loss >= validation_loss:
             num_epochs += 1
 
@@ -555,7 +608,7 @@ def main():
             epochs = [i for i in range(epoch + 1)]
             plot_loss(args, loss_per_epoch, epochs)
         print("Finished Training!")
-###########
+    ###########
 
     if args.validate:
         net.load_state_dict(torch.load(args.load_path), strict=False)
